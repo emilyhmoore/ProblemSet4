3ae84daa0c80bfc4e58c5d29baf338eefb2ca3fb
emilyhmoore <emily.moore@wustl.edu>
2014-02-19 20:48:22 -0600

    JMR CH 4 prob 4
    added above

--- PSMoore4.R
+++ PSMoore4.R
@@ -29,7 +29,19 @@ squarecube<-function(n){
 squarecube(7)
 
 ##Prob 4:
+##Function creates multiplication table for any value of n. 
+mult<-function(n){
+vect<-seq(1,n) ##Create first vector from 1 to n
+vect2<-seq(1,n) ##Do it again
+mat<-matrix(rep (0, n*n), nrow=n) ##create a matrix of 0s to fill in
+for(i in 1:n){
+mat[i,]<-vect[i]*vect2  ##fill in matrix row by row multiplying each element
+##of vector times entire vector
+}
+return(mat)
+}
 
+mult(9) ##Try it out
 
 ##Chapter 7 JMR Probs 3 and 4
 
