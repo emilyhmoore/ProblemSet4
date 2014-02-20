8bb424aefa87c80be2aaf65b6ed45bc6e0d890c4
emilyhmoore <emily.moore@wustl.edu>
2014-02-19 20:21:34 -0600

    JMR Problems
    Adds JMR ch 4 prob 3

--- PSMoore4.R
+++ PSMoore4.R
@@ -15,6 +15,24 @@ datetime<-gsub("/|:", ".", datetime)
 ##Make the directory name include the name and date/time
 dir_name<-paste(name, datetime)
 
+##Chapter 4 JMR Probs 3 and 4
+###Prob 3:
+squarecube<-function(n){
+  number<-1:n
+  square<-number^2
+  cube<-number^3
+  tab<-cbind(square, cube)
+  tab<-as.table(tab)
+  row.names(tab)<-1:n
+  return(tab)
+}
+squarecube(7)
+
+##Prob 4:
+
+
+##Chapter 7 JMR Probs 3 and 4
+
 
 ##Previous stuff from class to incorporate later. 
 
